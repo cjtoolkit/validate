e557a8add4235edf2fd5a7f0ab5dc06c6e7e7fa1
Christopher John Jackson <hello@cj-jackson.com>
2018-05-21 08:42:14 +0100

    Added clean and merge errors

--- vError/helpers.go
+++ vError/helpers.go
@@ -25,3 +25,34 @@ func Must(err error) {
 		panic(err)
 	}
 }
+
+func CleanError(err error) error {
+	switch err := err.(type) {
+	case Errors:
+		dupeList := map[string]bool{}
+		cleanErr := Errors{}
+
+		for _, e := range err {
+			if ve, ok := e.(ValidationError); ok {
+				key := ve.Type + ve.Format
+				if !dupeList[key] {
+					dupeList[key] = true
+					cleanErr = append(cleanErr, ve)
+				}
+			}
+		}
+
+		return cleanErr
+	}
+
+	return err
+}
+
+func MergeErrors(errs ...error) error {
+	collector := NewErrorCollector()
+	for _, err := range errs {
+		collector.Collect(err)
+	}
+
+	return CleanError(collector.GetErrors())
+}
