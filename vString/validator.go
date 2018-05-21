155e7f6e5ffd12144915bb70057778b25a81b7f7
Christopher John Jackson <hello@cj-jackson.com>
2018-05-21 10:00:31 +0100

    Added Validate Multiple

--- vString/validator.go
+++ vString/validator.go
@@ -3,6 +3,8 @@ package vString
 import (
 	"strings"
 
+	"sort"
+
 	"github.com/cjtoolkit/validate/vError"
 )
 
@@ -19,6 +21,26 @@ func Validate(value string, rules ...ValidationRule) (string, error) {
 	}
 
 	return value, collector.GetErrors()
+}
+
+func ValidateMultiple(values []string, rules ...ValidationRule) ([]string, error) {
+	m := map[string]bool{}
+
+	collector := vError.NewErrorCollector()
+
+	for _, value := range values {
+		value, err := Validate(value, rules...)
+		m[value] = true
+		collector.Collect(err)
+	}
+
+	cleanValue := []string{}
+	for key, _ := range m {
+		cleanValue = append(cleanValue, key)
+	}
+	sort.Strings(cleanValue)
+
+	return cleanValue, vError.CleanError(collector.GetErrors())
 }
 
 func Must(value string, err error) string {
