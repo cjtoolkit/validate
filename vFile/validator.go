0514262b227b53c08f16788241eae5ad97a62f2e
Christopher John Jackson <hello@cj-jackson.com>
2018-05-21 10:23:57 +0100

    Added file multiple

--- vFile/validator.go
+++ vFile/validator.go
@@ -20,7 +20,23 @@ func Validate(file *multipart.FileHeader, rules ...ValidationRule) (*multipart.F
 	return value, collector.GetErrors()
 }
 
+func ValidateMultiple(files []*multipart.FileHeader, rules ...ValidationRule) ([]*multipart.FileHeader, error) {
+	collector := vError.NewErrorCollector()
+
+	for _, file := range files {
+		_, err := Validate(file, rules...)
+		collector.Collect(err)
+	}
+
+	return files, vError.CleanError(collector.GetErrors())
+}
+
 func Must(file *multipart.FileHeader, err error) *multipart.FileHeader {
 	vError.Must(err)
 	return file
+}
+
+func MustMultiple(files []*multipart.FileHeader, err error) []*multipart.FileHeader {
+	vError.Must(err)
+	return files
 }
